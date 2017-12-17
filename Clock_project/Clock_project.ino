1f3caf012a47a3a81c30fd7a20c1dc7538eb8ee9
Kacper <kacper@kacpers-macbook.home>
2017-12-17 22:32:13 +0100

    begining of project

---
+++ Clock_project/Clock_project.ino
@@ -0,0 +1,22 @@
+#include <TM1637Display.h>
+
+//Data
+uint8_t hours, minutes, seconds;
+
+//Display
+#define CLK 2
+#define DIO 3
+
+TM1637Display display(CLK, DIO);
+
+//Setup of elements
+void setup()
+{
+  
+}
+
+//Operating system
+void loop()
+{
+  
+}
