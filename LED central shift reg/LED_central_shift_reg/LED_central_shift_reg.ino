23de32f2e371661a93dca58fab389ab85e16ad33
unknown <k.miziolek08@gmail.com>
2020-03-18 10:40:43 +0100

    Update of software

--- LED central shift reg/LED_central_shift_reg/LED_central_shift_reg.ino
+++ LED central shift reg/LED_central_shift_reg/LED_central_shift_reg.ino
@@ -2,6 +2,17 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+enum pilot
+{
+	ChanalDown, Chanal, ChanalUp,
+	Left, Right, Play,
+	Minus, Plus, EQ,
+	Zero, Hundred, TwoHundred,
+	One, Two, Three,
+	Four, Five, Six,
+	Seven, Eight, Nine
+};
+
 //IR config
 #define IR_SIG_pin 2
 IRrecv IR(IR_SIG_pin);
@@ -18,15 +29,24 @@ LiquidCrystal_I2C lcd(0x3F,16,2);
 
 
 //data
+#define display_options 2
+bool change_display = true, display_working = true;
+unsigned int display_state = 0;
+int button, state = 0;
+String time, reg;
+
 byte SR_data = 0xFF; //Shift reg data
 
 unsigned long IR_data; //IR data
 decode_results results; //decode IR data
 
 uint8_t hours, minutes, seconds; //DS3231 data
+uint8_t new_hours, new_minutes, new_seconds;
 
 
-void IR_change_value();
+void read_RTC();
+void set_RTC();
+void map_IR_code();
 
 
 void setup()
@@ -50,15 +70,12 @@ void setup()
 	//LCD
 	lcd.init();
 	lcd.backlight();
-	lcd.print("Działam23");
+	lcd.print("Start");
 
 	//set register
 	pinMode( SR_SHCP_pin, OUTPUT );
 	pinMode( SR_DS_pin, OUTPUT );
 	pinMode( SR_STCP_pin, OUTPUT );
-
-	//SERIAL
-	Serial.begin(9600);
   
 	//serial register start work
 	digitalWrite( SR_MR_pin, HIGH );
@@ -67,8 +84,8 @@ void setup()
 
 void loop()
 {
-	//Print config of reg
-	Serial.println(SR_data, BIN);
+	time = "    " + (hours < 10 ? (" " + String(hours)) : String(hours) ) + ":" + (minutes < 10 ? ("0" + String(minutes)) : String(minutes) ) + ":" + (seconds < 10 ? ("0" + String(seconds)) : String(seconds) );
+	reg = "    " + String(SR_data, BIN);
 
 	//sent data to register
 	digitalWrite( SR_STCP_pin, LOW );
@@ -81,10 +98,682 @@ void loop()
 		IR_data = results.value;
 		Serial.println(IR_data, HEX);
 		IR.resume();
-		delay(100);
+		map_IR_code();
 	}
 
 	//reading data form RTC
+	read_RTC();
+	
+
+	//state logic
+	if(change_display && display_working)
+	{
+		switch(display_state)
+		{
+			case 110:
+				lcd.clear();
+				lcd.print("    Settings");
+				lcd.setCursor(0,1);
+				lcd.print("1. Clock");
+				lcd.noCursor();
+				lcd.noBlink();
+				break;
+			case 111:
+				lcd.clear();
+				lcd.print("Hours");
+				lcd.setCursor(1,1);
+				lcd.cursor();
+				lcd.blink();
+				break;
+			case 112:
+				lcd.clear();
+				lcd.print("Hours");
+				lcd.setCursor(1,1);
+				lcd.cursor();
+				lcd.print(new_hours);
+				lcd.blink();
+				break;
+			case 113:
+				lcd.clear();
+				lcd.print("Hours");
+				lcd.setCursor(1,1);
+				lcd.noCursor();
+				lcd.print(new_hours);
+				lcd.noBlink();
+				break;
+			case 114:
+				lcd.clear();
+				lcd.print("Minutes");
+				lcd.setCursor(1,1);
+				lcd.cursor();
+				lcd.blink();
+				break;
+			case 115:
+				lcd.clear();
+				lcd.print("Minutes");
+				lcd.setCursor(1,1);
+				lcd.cursor();
+				lcd.print(new_minutes);
+				lcd.blink();
+				break;
+			case 116:
+				lcd.clear();
+				lcd.print("Minutes");
+				lcd.setCursor(1,1);
+				lcd.noCursor();
+				lcd.print(new_minutes);
+				lcd.noBlink();
+				break;
+			case 117:
+				lcd.clear();
+				lcd.print("Seconds");
+				lcd.setCursor(1,1);
+				lcd.cursor();
+				lcd.blink();
+				break;
+			case 118:
+				lcd.clear();
+				lcd.print("Seconds");
+				lcd.setCursor(1,1);
+				lcd.cursor();
+				lcd.print(new_seconds);
+				lcd.blink();
+				break;
+			case 119:
+				lcd.clear();
+				lcd.print("Seconds");
+				lcd.setCursor(1,1);
+				lcd.noCursor();
+				lcd.print(new_seconds);
+				lcd.noBlink();
+				break;
+			case 1:
+				lcd.clear();
+				lcd.print(time);
+				lcd.setCursor(0,1);
+				lcd.print(reg);
+				lcd.noCursor();
+				lcd.noBlink();
+				break;
+			case 0:
+			default:
+				lcd.clear();
+				lcd.print(time);
+				lcd.setCursor(0,1);
+				lcd.noCursor();
+				lcd.noBlink();
+				break;
+
+		}
+
+		change_display = false;
+	}
+	
+	switch(state)
+	{
+		case 110:
+			switch(button)
+			{
+				case EQ:
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Play:
+					state = 0;
+					display_state = 0;
+					change_display = true;
+					break;
+				default:
+					break;
+			}
+			break;
+		case 111:
+			switch(button)
+			{
+				case Zero:
+					new_hours = 0;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case One:
+					new_hours = 1;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Two:
+					new_hours = 2;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Play:
+					state = 110;
+					display_state = 110;
+					change_display = true;
+					break;
+				default:
+					break;
+			}
+			break;
+		case 112:
+			if(new_hours == 2)
+			{
+				switch(button)
+				{
+					case Zero:
+						new_hours = new_hours * 10 + 0;
+						state = state + 1;
+						display_state = display_state + 1;
+						change_display = true;
+						break;
+					case One:
+						new_hours = new_hours * 10 + 1;
+						state = state + 1;
+						display_state = display_state + 1;
+						change_display = true;
+						break;
+					case Two:
+						new_hours = new_hours * 10 + 2;
+						state = state + 1;
+						display_state = display_state + 1;
+						change_display = true;
+						break;
+					case Three:
+						new_hours = new_hours * 10 + 3;
+						state = state + 1;
+						display_state = display_state + 1;
+						change_display = true;
+						break;
+					case Left:
+						new_hours = new_hours / 10;
+						state = state -1;
+						display_state = display_state - 1;
+						change_display = true;
+						break;
+					case Play:
+						state = 110;
+						display_state = 110;
+						change_display = true;
+						break;
+					default:
+						break;
+				}
+			}else{
+				switch(button)
+				{
+					case Zero:
+						new_hours = new_hours * 10 + 0;
+						state = state + 1;
+						display_state = display_state + 1;
+						change_display = true;
+						break;
+					case One:
+						new_hours = new_hours * 10 + 1;
+						state = state + 1;
+						display_state = display_state + 1;
+						change_display = true;
+						break;
+					case Two:
+						new_hours = new_hours * 10 + 2;
+						state = state + 1;
+						display_state = display_state + 1;
+						change_display = true;
+						break;
+					case Three:
+						new_hours = new_hours * 10 + 3;
+						state = state + 1;
+						display_state = display_state + 1;
+						change_display = true;
+						break;
+					case Four:
+						new_hours = new_hours * 10 + 4;
+						state = state + 1;
+						display_state = display_state + 1;
+						change_display = true;
+						break;
+					case Five:
+						new_hours = new_hours * 10 + 5;
+						state = state + 1;
+						display_state = display_state + 1;
+						change_display = true;
+						break;
+					case Six:
+						new_hours = new_hours * 10 + 6;
+						state = state + 1;
+						display_state = display_state + 1;
+						change_display = true;
+						break;
+					case Seven:
+						new_hours = new_hours * 10 + 7;
+						state = state + 1;
+						display_state = display_state + 1;
+						change_display = true;
+						break;
+					case Eight:
+						new_hours = new_hours * 10 + 8;
+						state = state + 1;
+						display_state = display_state + 1;
+						change_display = true;
+						break;
+					case Nine:
+						new_hours = new_hours * 10 + 9;
+						state = state + 1;
+						display_state = display_state + 1;
+						change_display = true;
+						break;
+					case Left:
+						new_hours = new_hours / 10;
+						state = state -1;
+						display_state = display_state - 1;
+						change_display = true;
+						break;
+					case Play:
+						state = 110;
+						display_state = 110;
+						change_display = true;
+						break;
+					default:
+						break;
+				}
+			}
+			break;
+		case 113:
+			switch(button)
+			{
+				case EQ:
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Left:
+					new_hours = new_hours / 10;
+					state = state -1;
+					display_state = display_state - 1;
+					change_display = true;
+					break;
+				case Play:
+					state = 110;
+					display_state = 110;
+					change_display = true;
+					break;
+				default:
+					break;
+			}
+			break;
+		case 114:
+			switch(button)
+			{
+				case Zero:
+					new_minutes = 0;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case One:
+					new_minutes = 1;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Two:
+					new_minutes = 2;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Three:
+					new_minutes = 3;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Four:
+					new_minutes = 4;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Five:
+					new_minutes = 5;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Play:
+					state = 110;
+					display_state = 110;
+					change_display = true;
+					break;
+				default:
+					break;
+			}
+			break;
+		case 115:
+			switch(button)
+			{
+				case Zero:
+					new_minutes = new_minutes * 10 + 0;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case One:
+					new_minutes = new_minutes * 10 + 1;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Two:
+					new_minutes = new_minutes * 10 + 2;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Three:
+					new_minutes = new_minutes * 10 + 3;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Four:
+					new_minutes = new_minutes * 10 + 4;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Five:
+					new_minutes = new_minutes * 10 + 5;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Six:
+					new_minutes = new_minutes * 10 + 6;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Seven:
+					new_minutes = new_minutes * 10 + 7;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Eight:
+					new_minutes = new_minutes * 10 + 8;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Nine:
+					new_minutes = new_minutes * 10 + 9;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Left:
+					new_minutes = new_minutes / 10;
+					state = state -1;
+					display_state = display_state - 1;
+					change_display = true;
+					break;
+				case Play:
+					state = 110;
+					display_state = 110;
+					change_display = true;
+					break;
+				default:
+					break;
+			}
+			break;
+		case 116:
+			switch(button)
+			{
+				case EQ:
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Left:
+					new_minutes = new_minutes / 10;
+					state = state -1;
+					display_state = display_state - 1;
+					change_display = true;
+					break;
+				case Play:
+					state = 110;
+					display_state = 110;
+					change_display = true;
+					break;
+				default:
+					break;
+			}
+			break;
+
+		case 117:
+			switch(button)
+			{
+				case Zero:
+					new_seconds = 0;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case One:
+					new_seconds = 1;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Two:
+					new_seconds = 2;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Three:
+					new_seconds = 3;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Four:
+					new_seconds = 4;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Five:
+					new_seconds = 5;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Play:
+					state = 110;
+					display_state = 110;
+					change_display = true;
+					break;
+				default:
+					break;
+			}
+			break;
+		case 118:
+			switch(button)
+			{
+				case Zero:
+					new_seconds = new_seconds * 10 + 0;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case One:
+					new_seconds = new_seconds * 10 + 1;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Two:
+					new_seconds = new_seconds * 10 + 2;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Three:
+					new_seconds = new_seconds * 10 + 3;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Four:
+					new_seconds = new_seconds * 10 + 4;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Five:
+					new_seconds = new_seconds * 10 + 5;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Six:
+					new_seconds = new_seconds * 10 + 6;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Seven:
+					new_seconds = new_seconds * 10 + 7;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Eight:
+					new_seconds = new_seconds * 10 + 8;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Nine:
+					new_seconds = new_seconds * 10 + 9;
+					state = state + 1;
+					display_state = display_state + 1;
+					change_display = true;
+					break;
+				case Left:
+					new_seconds = new_seconds / 10;
+					state = state -1;
+					display_state = display_state - 1;
+					change_display = true;
+					break;
+				case Play:
+					state = 110;
+					display_state = 110;
+					change_display = true;
+					break;
+				default:
+					break;
+			}
+			break;
+		case 119:
+			switch(button)
+			{
+				case EQ:
+					state = 0;
+					display_state = 0;
+					set_RTC();
+					change_display = true;
+					break;
+				case Left:
+					new_seconds = new_seconds / 10;
+					state = state -1;
+					display_state = display_state - 1;
+					change_display = true;
+					break;
+				case Play:
+					state = 110;
+					display_state = 110;
+					change_display = true;
+					break;
+				default:
+					break;
+			}
+			break;
+		case 0:
+		default:
+			switch(button)
+			{
+				case Zero:
+					SR_data = 0xFF;
+					break;
+				case One:
+					SR_data = (SR_data & 0b11111110) + ((SR_data + 1) & 0x01);
+					break;
+				case Two:
+					SR_data = (SR_data & 0b11111101) + ((SR_data + 2) & 0x02);
+					break;
+				case Three:
+					SR_data = (SR_data & 0b11111011) + ((SR_data + 4) & 0x04);
+					break;
+				case Four:
+					SR_data = (SR_data & 0b11110111) + ((SR_data + 8) & 0x08);
+					break;
+				case Five:
+					SR_data = (SR_data & 0b11101111) + ((SR_data + 16) & 0x10);
+					break;
+				case Six:
+					SR_data = (SR_data & 0b11011111) + ((SR_data + 32) & 0x20);
+					break;
+				case Seven:
+					SR_data = (SR_data & 0b10111111) + ((SR_data + 64) & 0x40);
+					break;
+				case Eight:
+					SR_data = (SR_data & 0b01111111) + ((SR_data + 128) & 0x80);
+					break;
+				case Nine:
+					SR_data = 0x00;
+					break;
+				case Left:
+					display_state = (display_state - 1) % display_options;
+					change_display = true;
+					break;
+				case Right:
+					display_state = (display_state + 1) % display_options;
+					change_display = true;
+					break;
+				case Play:
+					if(display_working)
+					{
+						lcd.noDisplay();
+						lcd.noBacklight();
+						display_working = false;
+					}else{
+						lcd.backlight();
+						lcd.display();
+						display_working = true;
+					}
+					break;
+				case EQ:
+					state = 110;
+					display_state = 110;
+					change_display = true;
+					break;
+				case -1:
+				default:
+					break;
+			}
+	}
+
+	button = -1;
+}
+
+void read_RTC()
+{
+	int seconds_old = seconds;
 	Wire.beginTransmission(0x68); //It's an address of RTC DS3231
 	Wire.write(0);
 	Wire.endTransmission();
@@ -102,59 +791,98 @@ void loop()
 		hours = (((hours & 0b00100000) >> 5) * 20 + ((hours & 0b00010000) >> 4) * 10 + (hours & 0b00001111));
 	}
 
-	IR_change_value();
+	if(seconds != seconds_old)
+		change_display = true;
 }
 
-void IR_change_value()
+void set_RTC()
+{
+	new_hours = 0b00000000 + (((new_hours / 10) << 4) & 0b00110000) + ((new_hours % 10) & 0b00001111);;
+	new_minutes = 0b00000000 + (((new_minutes / 10) << 4) & 0b01110000) + ((new_minutes % 10) & 0b00001111);
+	new_seconds = 0b00000000 + (((new_seconds / 10) << 4) & 0b01110000) + ((new_seconds % 10) & 0b00001111);
+	//HOURS
+	Wire.beginTransmission(0x68);
+	Wire.write(0x02);
+	Wire.write(new_hours);
+	Wire.endTransmission();
+	//MINUTES
+	Wire.beginTransmission(0x68);
+	Wire.write(0x01);
+	Wire.write(new_minutes);
+	Wire.endTransmission();
+	//SECONDS
+	Wire.beginTransmission(0x68);
+	Wire.write(0x00);
+	Wire.write(new_seconds);
+	Wire.endTransmission();
+}
+
+void map_IR_code()
 {
 	switch(IR_data)
 	{
 		case 0xFFA25D:  //CH-   0xFFA25D
+			button = ChanalDown;
 			break;
 		case 0xFF629D:  //CH  0xFF629D
+			button = Chanal;
 			break;
 		case 0xFFE21D:  //CH+   0xFFE21D
+			button = ChanalUp;
 			break;
 		case 0xFF22DD:  //<<  0xFF22DD
+			button = Left;
 			break;
 		case 0xFF02FD:  //>>  0xFF02FD
+			button = Right;
 			break;
 		case 0xFFC23D:  //>||   0xFFC23D
+			button = Play;
 			break;
 		case 0xFFE01F:  //–   0xFFE01F
+			button = Minus;
 			break;
 		case 0xFFA857:  //+   0xFFA857
+			button = Plus;
 			break;
 		case 0xFF906F:  //EQ  0xFF906F
+			button = EQ;
 			break;
 		case 0xFF9867:  //100+  0xFF9867
-			SR_data = 0x70;
+			button = Hundred;
 			break;
 		case 0xFFB04F:  //200+  0xFFB04F
-			SR_data = 0x00;
+			button = TwoHundred;
 			break;
 		case 0xFF6897:  //0   0xFF6897
-			SR_data = 0xFF;
+			button = Zero;
 			break;
 		case 0xFF30CF:  //1   0xFF30CF
-			SR_data = (SR_data & 0b11111110) + ((SR_data + 1) & 0x01);
+			button = One;
 			break;
 		case 0xFF18E7:  //2   0xFF18E7
-			SR_data = (SR_data & 0b11111101) + ((SR_data + 2) & 0x02);
+			button = Two;
 			break;
 		case 0xFF7A85:  //3   0xFF7A85
+			button = Three;
 			break;
 		case 0xFF10EF:  //4   0xFF10EF
+			button = Four;
 			break;
 		case 0xFF38C7:  //5   0xFF38C7
+			button = Five;
 			break;
 		case 0xFF5AA5:  //6   0xFF5AA5
+			button = Six;
 			break;
 		case 0xFF42BD:  //7   0xFF42BD
+			button = Seven;
 			break;
 		case 0xFF4AB5:  //8   0xFF4AB5
+			button = Eight;
 			break;
 		case 0xFF52AD:  //9   0xFF52AD
+			button = Nine;
 			break;
 	}
 }
